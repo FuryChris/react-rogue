c99eeec5272853ff023eef1fb22db365f08d5f03
krzysiek2222 <desmont2222@gmail.com>
2020-05-07 19:57:57 +0200

    First Commit
    Basic setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactRogue from './ReactRogue.js';
+
+const App = () =>  (
+<div className="App">
+  <h1>Tytułdsa sad asd aaaaaaaaaaaaaa</h1>
+  <p>Zawartosci jakis tekst łeeesadase</p>
+  <ReactRogue />
+  </div>
+)
+
+export default App;
